c403360b6d12e97d813729e769e6cc4e783e0ffe
Alina <example@gmail.com>
2023-05-31 12:34:39 -0500

    first commit

---
+++ jss/script.js
@@ -0,0 +1,71 @@
+// let number = 4.6;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = "Alex";
+
+// const bool = false;
+
+
+// let und;
+// console.log(und);
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+
+// console.log(obj['name']);
+
+//  let arr = ['plum.png','orange.jpn','apple.bmp'];
+//  console.log(arr[1]);
+
+  
+//  const obj = {
+//     'Anna': 200,
+//     'Alica':300
+//  };
+
+//  const arr = ['a','b','c'];
+//  const arrobj = {
+//     a:'a';
+
+//  }
+
+// const b ='b';
+// arrobj['b'] = '1234';
+// console.log(arrobj['b']);
+
+// const storName = 'Dart';
+// const storeDescription = {
+//     'budget': 10000,
+//     'employees': ['John','Eva','Ava'],
+//     'products':{
+//         'milk': 100,
+//         'apple': 150
+//     },
+//     'open':true
+// };
+
+
+// alert('hello');
+// const result = confirm('Are you here?');
+
+const answers = [];
+
+// answers[0] = prompt('How is your name?','');
+// answers[1] = prompt('How is your last name?','');
+// answers[2] = prompt('How are you?','');
+
+console.log(typeof(answers));
+console.log(typeof(null));
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+alert(`hay, ${user}`);
